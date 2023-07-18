87c8dc04aab6d91f69253253c2f0f82ae23be85b
James Ross <james@jross.me>
2023-07-18 21:50:53 +0100

    feat: add fullstack challenge (#1)
    Co-authored-by: Jonathan Yarbor <jon@nodecraft.com>

---
+++ full-stack/functions/api/users.ts
@@ -0,0 +1,18 @@
+export const onRequestGet: PagesFunction = async (context) => {
+	const url = new URL(context.request.url);
+
+	// retrieve 10 users by default, allow override with maxUsers query param
+	let maxUsers = 10;
+	if(url.searchParams.has('maxUsers')) {
+		maxUsers = Number.parseInt(url.searchParams.get('maxUsers') as string);
+	}
+
+	// loop and get users
+	const users = [];
+	for(let i = 0; i < maxUsers; i++) {
+		const userRes = await fetch('https://randomuser.me/api/');
+		const user = await userRes.json<any>();
+		users.push(user.results[0]);
+	}
+	return Response.json(users);
+};
